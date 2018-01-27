ecb3fe8df7d043459babd604af95a39ae5ca40ba
romqu <roman.quistler@gmail.com>
2018-01-27 17:40:37 +0100

    push by push

--- src/model/defect.ts
+++ src/model/defect.ts
@@ -1,9 +1,12 @@
+import { Image } from "./image";
+
 export class Defect {
   constructor(
     private readonly params: {
       readonly description: string;
       readonly personInCharge: string;
       readonly doneTill: string;
+      readonly images: Image[];
     }
   ) {}
 }
--- src/model/room.ts
+++ src/model/room.ts
@@ -1,9 +1,12 @@
+import { Defect } from "./defect";
+
 export class Room {
   constructor(
     private readonly params: {
       readonly name: string;
       readonly number: number;
       readonly description: string;
+      readonly defects: Defect[];
     }
   ) {}
 }
