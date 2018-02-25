341700c513891c3882e7b8a5d2ec15f29d31c9af
romqu <roman.quistler@gmail.com>
2018-02-25 22:49:25 +0100

    push

--- sql/03-queries.sql
+++ sql/03-queries.sql
@@ -7,17 +7,6 @@ FROM
     INNER JOIN living_unit ON living_unit.floor_id = floor.id
     INNER JOIN room ON room.id = living_unit.id
     INNER JOIN defect ON defect.room_id = room.id
-    INNER JOIN defect_image ON defect_image.defect_id = defect.id;
-
-SELECT
-    *
-FROM
-    client
-    INNER JOIN street_address ON client.id = 1
-        AND street_address.client_id = client.id
-    INNER JOIN floor ON floor.street_address_id = street_address.id
-    INNER JOIN living_unit ON living_unit.floor_id = floor.id
-    INNER JOIN room ON room.living_unit_id = living_unit.id
-    INNER JOIN defect ON defect.room_id = room.id
-    INNER JOIN defect_image ON defect_image.defect_id = defect.id;
+    INNER JOIN defect_image ON defect_image.defect_id = defect.id
+WHERE street_address.client_id = 1;
 
