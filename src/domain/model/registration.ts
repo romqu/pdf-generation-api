cb6fadebbe229f37afb5feaed29624dd00df4bec
romqu <roman.quistler@gmail.com>
2018-03-16 23:12:48 +0100

    push

--- src/domain/model/registration.ts
+++ src/domain/model/registration.ts
@@ -1,5 +1,12 @@
 import { LoginCredentials } from "../../domain/model/loginCredentials";
+import { Client } from "./client";
 
 export class Registration {
   public readonly loginCredentials: LoginCredentials;
+  public readonly client: Client;
+
+  constructor(loginCredentials: LoginCredentials, client: Client) {
+    this.loginCredentials = loginCredentials;
+    this.client = client;
+  }
 }
